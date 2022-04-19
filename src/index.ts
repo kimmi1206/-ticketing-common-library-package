a61343021986b8ffd8babd37e677482c2d6b1259
kimmi1206 <kheinerimm1206@gmail.com>
2022-04-19 12:21:17 -0500

    Updates

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 15,
+  green: 25,
+  blue: 35,
+};
+
+console.log(color);
+
+export default color;
